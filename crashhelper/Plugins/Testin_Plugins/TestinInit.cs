019c801f3f8425d8ba503950c9b3623e42f32beb
james-Xuan <xuan.android.love@gmail.com>
2014-12-08 12:01:34 +0800

    delete Hyperlinks

--- crashhelper/Plugins/Testin_Plugins/TestinInit.cs
+++ crashhelper/Plugins/Testin_Plugins/TestinInit.cs
@@ -7,7 +7,6 @@ public class TestinInit : MonoBehaviour {
 	/// Your Testin Appkey.  Every app has a special identifier that allows Testin Crash
 	/// to associate error monitoring data with your app.  Your Appkey can be found on the
 	/// "App Settings" page of the app you are trying to monitor.
-	/// See the Testin Crash portal http://crash.testin.cn
 	/// </summary>
 	/// <example>A real Appkey looks like this:  ec0cd05fb39700ba42a43eca799e7528</example>
 	private const string TestinAppKey = "your appKey";
